d906e83c34979c5f889077955e8bbc15a8f3e304
Ramesh Ghimire <rameshxghimire@gmail.com>
2018-11-26 22:16:12 +0200

    The script file.

---
+++ content-analyst.py
@@ -0,0 +1,8 @@
+"""
+This program will look for a text file in the program's directory or given alternative
+location and do it's job using the Natural Language Processing. 
+This will be our primary script file.
+"""
+# Work halted for further preparation
+# TODO: Create a dedicated local programming environment and install required modules before
+# before proceeding.
